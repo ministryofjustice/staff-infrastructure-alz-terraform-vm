2a4f3f0186b4f080f4261c6248a7e478a56dec85
Dan Lumb <daniel.lumb@justice.gov.uk>
2022-09-29 12:23:28 +0100

    initial commit

---
+++ alz-win-vm/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">=1.2.6"
+  experiments      = [module_variable_optional_attrs]
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.14.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.3.0"
+    }
+  }
+}
