c56811fc4f9ae94bb6829737071337bdd4e84c35
Dan Lumb <daniel.lumb@justice.gov.uk>
2022-09-30 17:35:14 +0100

    Setting sub id and tenant id as vars for pipeline

---
+++ tests/terraform/vm-build/variables.tf
@@ -0,0 +1,11 @@
+# Pipeline requires these be set
+
+variable "subscription_id" {
+  description = "Subscription ID"
+  default     = "4b068872-d9f3-41bc-9c34-ffac17cf96d6" # devl testing
+}
+
+variable "tenant_id" {
+  description = "Tenant ID"
+  default     = "0bb413d7-160d-4839-868a-f3d46537f6af" # dev
+}
