877fbd879107565e7341b6a1a24c1559c14c1a39
Dan Lumb <daniel.lumb@justice.gov.uk>
2022-12-01 12:02:09 +0000

    typo in tests

--- tests/tests.ps1
+++ tests/tests.ps1
@@ -31,6 +31,6 @@ Describe 'ALZ VM Module validation' {
         It "Windows VM exists with correct name" { $vmWin.Name | Should -Be "vm-test-win-01" }
         It "Windows VM has correct IP address" { $winNic.IPConfigurations.PrivateIPAddress | Should -Be "192.168.99.5" }
         It "Windows VM has created credentials in Keyvault" { $kvSecrets | Should -Contain "vm-test-win-01-password" }
-        It "Windows VM has monitoring extension provisioned" { $monitorStatus | Should be "Succeeded" }
+        It "Windows VM has monitoring extension provisioned" { $monitorStatus | Should -Be "Succeeded" }
     }
 }
