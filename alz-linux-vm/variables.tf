97686e2cd661d44b39c80ea751e29582955ae6ef
raghuvanshir <ravindra.raghuvanshi@justice.gov.uk>
2023-07-21 07:13:05 +0100

    tags

--- alz-linux-vm/variables.tf
+++ alz-linux-vm/variables.tf
@@ -94,3 +94,14 @@ variable "recovery_vault_resource_group" {
   default     = null
 }
 
+variable "tags" {
+  description = "Standard tags for ALZ resources"
+  type = map(string)
+  default = {
+    application  = "Azure-Landing-Zone"
+    businessunit = "HQ"
+    isproduction = "True"
+    owner        = "PTTP Azure Landing Zone Team: PTTP3LSAzure@justice.gov.uk"
+    source-code  = "https://github.com/ministryofjustice/staff-infrastructure-azure-landing-zone"
+}
+}
