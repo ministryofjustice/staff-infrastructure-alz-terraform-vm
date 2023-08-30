b761a393aab46401b1811eb0aca8d23d9116649f
raghuvanshir <ravindra.raghuvanshi@justice.gov.uk>
2023-08-30 11:46:12 +0100

    testing 1.7

--- alz-win-vm/main.tf
+++ alz-win-vm/main.tf
@@ -218,24 +218,24 @@ resource "azurerm_virtual_machine_extension" "alz_win_antivirus" {
   virtual_machine_id         = azurerm_windows_virtual_machine.alz_win[each.key].id
   publisher                  = "Microsoft.Azure.Security"
   type                       = "IaaSAntimalware"
-  type_handler_version       = "1.1"
+  type_handler_version       = "1.7"
   tags                       = each.value.tags
   auto_upgrade_minor_version = "true"
   settings = jsonencode({
-  AntimalwareEnabled = true,
-  RealtimeProtectionEnabled = "true",
-  ScheduledScanSettings = {
-    isEnabled = "true",
-    day       = "7",
-    time      = "120",
-    scanType  = "Quick"
-  },
-  Exclusions = lookup(local.vm_specifications[each.key], "antimalware_exclusions", {
-    Extensions = ""
-    Paths      = "C:\\Windows\\SoftwareDistribution\\Datastore;C:\\Windows\\SoftwareDistribution\\Datastore\\Logs;C:\\Windows\\Security\\Database"
-    Processes  = "NTUser.dat*"
+    AntimalwareEnabled        = true,
+    RealtimeProtectionEnabled = "true",
+    ScheduledScanSettings = {
+      isEnabled = "true",
+      day       = "7",
+      time      = "120",
+      scanType  = "Quick"
+    },
+    Exclusions = lookup(local.vm_specifications[each.key], "antimalware_exclusions", {
+      Extensions = ""
+      Paths      = "C:\\Windows\\SoftwareDistribution\\Datastore;C:\\Windows\\SoftwareDistribution\\Datastore\\Logs;C:\\Windows\\Security\\Database"
+      Processes  = "NTUser.dat*"
+    })
   })
-})
 
 }
 
